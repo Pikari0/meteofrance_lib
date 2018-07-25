edfb9d514a2cfe5800c46a0e6aae27711024a9e8
Pikari0 <services@spika.xyz>
2018-07-25 13:05:38 +0200

    Initial commit

---
+++ meteofrance.py
@@ -0,0 +1,44 @@
+import requests
+import re
+
+
+def previsions(lieu):
+    r = requests.get("http://www.meteofrance.com/previsions-meteo-france"+lieu)
+
+    retour = {'previsions':[]}
+
+    page = r.text
+    
+    # on extrait les prévisions par jours
+    parties=page.split('prevision-horaire')
+
+    # parties[0] -> jours de la semaine
+    # parties[>0] -> par heure    
+
+
+    #### à récupérer 
+    # le libellé de la météo
+    # la température
+    # ressenti
+    # UV
+    # vent	
+    ## c'est dans <ul class="day-data">
+    
+    aujourdhui = parties[1]
+    heures = aujourdhui.split('<button class="time-range-trigger">')    
+
+    for heure in heures[1:]:
+        jour = re.search(r'<h3>([A-zÀ-ÿ0-9]|\s)*',heure).group(0).replace("<h3>",'')
+        horaire = re.search(r'<time datetime="[0-9]*h',heure).group(0).replace('<time datetime="','')
+        meteo = re.search(r'<li class="day-summary-label">([A-zÀ-ÿ0-9]|\s)*',heure).group(0).replace('<li class="day-summary-label"> ','')
+        temperature = re.search(r'<li class="day-summary-temperature"> [0-9]*°C',heure).group(0).replace('<li class="day-summary-temperature"> ','')
+        ressenti = re.search(r'Ressenti</abbr> [0-9]*°C',heure).group(0).replace('Ressenti</abbr> ','')
+        vent = re.search(r'[0-9]* km\/h',heure).group(0).replace('</span>','')
+        uv = re.search(r'<li class="day-summary-uv">UV [0-9]*',heure).group(0).replace('<li class="day-summary-uv">','')
+
+        tab = {'jour':jour,'horaire':horaire,'meteo':meteo,'temperature':temperature,'ressenti':ressenti,'vent':vent,'uv':uv}
+        retour['previsions'].append(tab)
+    return retour
+
+print(previsions("/lyon/69000"))
+
